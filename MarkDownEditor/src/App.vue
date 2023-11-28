5bda73dc21564c7d3c54d76e6d4e794e250681d7
Kiodayo <1498183375@qq.com>
2023-11-28 12:57:30 +0800

    Add Title

--- MarkDownEditor/src/App.vue
+++ MarkDownEditor/src/App.vue
@@ -1,10 +1,13 @@
 <!-- 编辑器开发 -->
 <template>
+    <h1 class="title">MarkDownEditor</h1>
     <div class="editor">
+
         <!-- input后，内容执行update -->
+
         <textarea class="input" :value="input" @input="update">
-                    <div class="output" v-html="output"></div>
-                    </textarea>
+                                                                    </textarea>
+        <div class="output" v-html="output"></div>
     </div>
 </template>
 
@@ -14,7 +17,7 @@ import { debounce } from 'lodash-es'
 import { ref, computed } from 'vue'
 
 // ref响应式的内容
-const input = ref('#hello')
+const input = ref('# hello')
 
 const output = computed(() => marked(input.value))
 
@@ -24,4 +27,48 @@ const update = debounce((e) => {
 </script>
 
 <!-- 样式 -->
-<style></style>
+<style>
+body {
+    margin: 0
+}
+
+.editor {
+    display: flex;
+    height: 100vh;
+}
+
+.title {
+    display: flex;
+    line-height: 30px;
+    height: 35px;
+    text-align: left;
+    width: 100%;
+    padding-left: 10px;
+
+}
+
+.input,
+.output {
+    overflow: auto;
+    width: 50%;
+    height: 100%;
+    border-top: 10px solid #ccc;
+    box-sizing: border-box;
+    padding: 0 20px;
+}
+
+.input {
+    border: none;
+    border-top: 10px solid #ccc;
+    border-right: 4px solid #ccc;
+    resize: none;
+    outline: none;
+    background-color: #f6f6f6;
+    font-size: 14px;
+    padding: 20px;
+}
+
+code {
+    color: #f66;
+}
+</style>
