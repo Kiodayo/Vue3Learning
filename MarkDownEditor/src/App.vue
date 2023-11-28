724366325d9c11e987b973f3930873f403f47150
Kiodayo <1498183375@qq.com>
2023-11-28 12:19:37 +0800

    Editor Dev

--- MarkDownEditor/src/App.vue
+++ MarkDownEditor/src/App.vue
@@ -1,9 +1,27 @@
-<template></template>
+<!-- 编辑器开发 -->
+<template>
+    <div class="editor">
+        <!-- input后，内容执行update -->
+        <textarea class="input" :value="input" @input="update">
+                    <div class="output" v-html="output"></div>
+                    </textarea>
+    </div>
+</template>
 
 <script setup>
 import { marked } from 'marked'
 import { debounce } from 'lodash-es'
 import { ref, computed } from 'vue'
 
+// ref响应式的内容
+const input = ref('#hello')
 
+const output = computed(() => marked(input.value))
+
+const update = debounce((e) => {
+    input.value = e.target.value
+})
 </script>
+
+<!-- 样式 -->
+<style></style>
